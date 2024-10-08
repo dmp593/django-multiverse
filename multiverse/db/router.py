73aad695037c13c41455e341e65b66c3fd2f6fa0
Daniel Pinto <daniel.pinto@setwin.pt>
2024-10-08 10:39:19 +0100

    improved DB Router for common apps

--- multiverse/db/router.py
+++ multiverse/db/router.py
@@ -1,7 +1,7 @@
 from django.db import DEFAULT_DB_ALIAS
 
 from multiverse.test import is_test_environment
-from multiverse.utils import get_tenant_database_alias, get_system_apps, get_common_apps
+from multiverse.utils import get_tenant, get_tenant_apps, get_tenant_database_alias, get_system_apps, get_common_apps
 from multiverse.awareness import get_current_tenant
 
 
@@ -25,6 +25,16 @@ class TenantRouter:
         app_label = model._meta.app_label
 
         if self.app_label_in_apps(app_label, get_common_apps()):
+            if 'relation' in hints:
+                relation = hints['relation']
+                relation_app_label = relation._meta.app_label
+
+                if self.app_label_in_apps(relation_app_label, get_system_apps()):
+                    return DEFAULT_DB_ALIAS
+
+                if self.app_label_in_apps(relation_app_label, get_tenant_apps()):
+                    return get_tenant_database_alias()
+
             return None
 
         if self.app_label_in_apps(app_label, get_system_apps()):
@@ -39,8 +49,8 @@ class TenantRouter:
         return self.db_for('write', model, **hints)
 
     def allow_relation(self, obj1, obj2, **hints):
-        db_obj1 = hints.get('database', None) or self.db_for_read(obj1)
-        db_obj2 = hints.get('database', None) or self.db_for_read(obj2)
+        db_obj1 = hints.get('database', obj1._state.db) or self.db_for_read(obj1, relation=obj2)
+        db_obj2 = hints.get('database', obj2._state.db) or self.db_for_read(obj2, relation=obj1)
 
         return db_obj1 and db_obj2 and db_obj1 == db_obj2
 
